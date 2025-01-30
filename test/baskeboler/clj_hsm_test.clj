b90073509579a28840389d088a2d4eedf1db7706
victor gil <baskeboler@gmail.com>
2025-01-30 08:02:10 -0300

    test

--- test/baskeboler/clj_hsm_test.clj
+++ test/baskeboler/clj_hsm_test.clj
@@ -13,11 +13,11 @@
   (:require
    [clojure.test :refer [deftest is testing]]
    [baskeboler.hsm.core :as hsm])
-  (:refer [baskeboler.hsm.protocol]))
+  (:refer [baskeboler.hsm.protocol ]))
 
-(deftest hsm-test 
+(deftest hsm-test
   (testing "create-hms"
     (let [device (hsm/create-hms)]
-      (is (some? device)) )))
+      (is (some? device)))))
 
 
